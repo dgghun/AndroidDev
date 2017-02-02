0df17731f37e83c7a3df309b4aed33d475ee018d
dgghun <gabriel3052000@yahoo.com>
2017-02-01 23:23:04 -0800

    Button and 2 textviews added with touchme repsonse

--- HelloWorld/app/src/main/java/regis/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/regis/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package regis.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    String nameStatic = new String();
+
+    public void touchMe(View view){
+        TextView tv = (TextView)findViewById(R.id.TV_paragraph);
+        TextView name = (TextView)findViewById(R.id.TV_enterName);
+
+
+        if(name.getText().toString().isEmpty())
+            tv.setText("Whats your name?");
+        else if(!nameStatic.equals(name.getText().toString()))
+            tv.setText("Whats up " + name.getText() + "!");
+        else
+            tv.setText("stop touching me!!");
+
+        nameStatic = name.getText().toString();
     }
 }
