94056ea4c6715e09b8fac3b19242a4c39336401e
DAVID <gabriel3052000@yahoo.com>
2017-03-21 10:49:46 -0700

    Android Enterprise Firebase project- NEW

---
+++ Project_Firebase/app/src/main/java/com/dgarcia/project_firebase/MainFragment.java
@@ -0,0 +1,24 @@
+package com.dgarcia.project_firebase;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstances){
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}
